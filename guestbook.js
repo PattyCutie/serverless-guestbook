295fc1373632676d0db4d385c3e78d16a3a9f243
PattyCutie <91567051+PattyCutie@users.noreply.github.com>
2022-01-12 14:57:40 +0100

    Update apiURL03

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d7f87ea5.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://62262f93.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
